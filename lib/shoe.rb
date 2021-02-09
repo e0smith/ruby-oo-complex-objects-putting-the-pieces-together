47414e4687525d42c0ad024ff422fc5263eba000
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-09 09:59:40 -0500

    commit tree f35de591696f97406bdcbe9ef20a4a7a2ab1abe4

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,60 @@
 # Make your shoe class here!
+class Shoe 
+    attr_accessor :color, :size, :condition, :material
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
+
+
+    # def brand=(brand)
+    #     @brand = brand
+    # end
+
+    # def brand
+    #     @brand
+    # end
+
+    # def color=(color)
+    #     @color = color
+    # end
+
+    # def color
+    #     @color
+    # end
+
+    # def size=(size)
+    #     @size = size
+    # end
+
+    # def size
+    #     @size
+    # end
+
+    # def material=(material)
+    #     @material = material
+    # end
+
+    # def material
+    #     @material
+    # end
+
+    # def condition=(condition)
+    #     @condition = condition
+    # end
+
+    # def condition
+    #     @condition
+    # end
+
+    # def cobble
+    #     puts "Your shoe is as good as new!"
+    # end
+# end
